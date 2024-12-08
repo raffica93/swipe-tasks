8c208bf354194eac30522c3e2b19098f4da66dd6
raffica93 <raffaeleliporace@gmail.com>
2024-12-08 23:47:24 +0100

    ecco esilint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   output: 'standalone',
   poweredByHeader: false,
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: false,
+    dirs: ['src']
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
